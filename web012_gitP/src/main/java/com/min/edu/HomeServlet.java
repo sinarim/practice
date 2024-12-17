8bc5803ce0ef320022679b8255a31e84fc01b2f8
sinarim <mw7813@naver.com>
2024-12-17 17:28:46 +0900

    연습

---
+++ web012_gitP/src/main/java/com/min/edu/HomeServlet.java
@@ -0,0 +1,20 @@
+package com.min.edu;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/HomeServlet.do")
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("./WEB-INF/views/home.jsp").forward(request, response);
+	}
+
+}
